f1dffa2fc33afc375d4efd6f8b188d749c9ccdaa
Jacob <jepartridge@shockers.wichita.edu>
2026-03-25 08:54:08 -0500

    created backend function for changing GUI spice layout, added salt teaspoons per second, added error message for spice time calculation, added type hints to functions author: Jacob Partridge

--- backEnd.py
+++ backEnd.py
@@ -1,5 +1,5 @@
 import time
-from adafruit_servokit import ServoKit
+# from adafruit_servokit import ServoKit
 
 class SpiceItUpBackend:
     def __init__(self):
@@ -7,73 +7,89 @@ class SpiceItUpBackend:
             print("Initializing backend...")
             """ salt, black pepper, garlic powder, onion powder, paprika, cumin, chili powder, cayenne pepper, dried oregano, brown sugar """
             # Initialize the kit.
-            kit = ServoKit(channels=16)
+            # kit = ServoKit(channels=16)
             #  Access the continuous rotation servo property on channel input
-            self.turnServo = kit.continuous_servo
+            # self.turnServo = kit.continuous_servo
             self.spiceBox = 0
             self.timeToRun = 0
             self.channel = 0
             self.spiceQueue = []
             self.recipes = {}
-            self.spices = { 'Spice 1': {"teaspoons/second" : 1.0, # Fine Salt
-                                    "currentlyHoused" : 1,
+            self.spices = { 'salt': {"teaspoons/second" : 7.5, # Fine Salt
+                                    "currentlyHoused" : 0,
                                     "conversionConstant" : 1},
 
-                            'Spice 2': {"teaspoons/second" : 1.0, # Black Pepper
-                                    "currentlyHoused" : 2,
+                            'black pepper': {"teaspoons/second" : 1.0, # Black Pepper
+                                    "currentlyHoused" : 0,
                                     "conversionConstant" : 1},
 
-                            'Spice 3': {"teaspoons/second" : 1.0, # Garlic Powder
-                                    "currentlyHoused" : 3,
+                            'garlic powder': {"teaspoons/second" : 1.0, # Garlic Powder
+                                    "currentlyHoused" : 0,
                                     "conversionConstant" : 1},
 
-                            'Spice 4': {"teaspoons/second" : 1.0, # Onion Powder
-                                    "currentlyHoused" : 4,
+                            'onion powder': {"teaspoons/second" : 1.0, # Onion Powder
+                                    "currentlyHoused" : 0,
                                     "conversionConstant" : 1},
 
-                            'Spice 5': {"teaspoons/second" : 1.0, # Paprika
-                                    "currentlyHoused" : 5,
+                            'paprika': {"teaspoons/second" : 1.0, # Paprika
+                                    "currentlyHoused" : 0,
                                     "conversionConstant" : 1},
 
-                            'Spice 6': {"teaspoons/second" : 1.0, # Cumin
-                                    "currentlyHoused" : 6,
+                            'cumin': {"teaspoons/second" : 1.0, # Cumin
+                                    "currentlyHoused" : 0,
                                     "conversionConstant" : 1},
 
-                            'Spice 7': {"teaspoons/second" : 1.0, # Chili Powder
-                                    "currentlyHoused" : 7,
+                            'chili powder': {"teaspoons/second" : 1.0, # Chili Powder
+                                    "currentlyHoused" : 0,
                                     "conversionConstant" : 1},
 
-                            'Spice 8': {"teaspoons/second" : 1.0, # Cayenne Pepper
-                                    "currentlyHoused" : 8,
+                            'cayenne pepper': {"teaspoons/second" : 1.0, # Cayenne Pepper
+                                    "currentlyHoused" : 0,
                                     "conversionConstant" : 1},
 
-                            'Spice 9': {"teaspoons/second" : 1.0, # Dried Oregano
-                                    "currentlyHoused" : 9,
+                            'dried oregano': {"teaspoons/second" : 1.0, # Dried Oregano
+                                    "currentlyHoused" : 0,
                                     "conversionConstant" : 1},
                                             
-                            'Spice 10': {"teaspoons/second" : 1.0, # Brown Sugar
-                                        "currentlyHoused" : 10,
+                            'brown sugar': {"teaspoons/second" : 1.0, # Brown Sugar
+                                        "currentlyHoused" : 0,
                                         "conversionConstant" : 1}
         }
             print("Backend initialized successfully.")
         except:
             print("Error initializing backend.")
             return
-        
         return
 
-    def addSpice(self, spiceName, teaspoonsPerSecond, currentlyHoused):
+    def addSpice(self, spiceName: str, teaspoonsPerSecond: float, currentlyHoused: int):
         self.spices[spiceName] = { "teaspoons/second" : teaspoonsPerSecond, "currentlyHoused" : currentlyHoused}
         return
 
-    def addRecipe(self, recipeName, spiceList):
+    def addRecipe(self, recipeName: str, spiceList: list):
         self.recipes[recipeName] = spiceList
+        return
+    
+    def calculateSpiceTime(self, amount: float, size: str, teaspoonsPerSecond: float):
+        if size == "Tablespoons":
+            return (amount * 3) * teaspoonsPerSecond
+        elif size == "Cups":
+            return (amount * 48) * teaspoonsPerSecond
+        else:
+            print("Error in spice time calculation")
+            return 0
+        
+    def changeSpiceLayouts(self, newLayout: dict):
+        # Assuming we are using the list to update the spice layout in the order of the spice dictionary
+        # As well as changing the buttons in the frontend to reflect the new spice layout
+        for spice in self.spices:
+            self.spices[spice]['currentlyHoused'] = newLayout[spice]
+            spiceArrayForFrontend = [spice for spice in self.spices]
         return
 
     def despenseSpice(self, spice: str, amount: str, size: str):
         self.spiceBox = self.spices[f'{spice}']
         self.housed = self.spiceBox['currentlyHoused']
-        amount = float(amount)
+        amount = int(amount)
         print(f"Box: {self.housed}\nAmount: {amount}\nSize: {size}\n")
         if self.housed == 0:
             print(f"Spice '{spice}' not housed, please house spice and try again.")
@@ -96,25 +112,17 @@ class SpiceItUpBackend:
             self.timeToRun = self.calculateSpiceTime(amount, size, self.spiceBox['teaspoons/second'])
                 
         if self.spiceBox['currentlyHoused'] % 2 == 0:
-            self.turnServo[self.channel].throttle = 0.2
+            # self.turnServo[self.channel].throttle = 0.2
             print("Turning servo forward...")
             time.sleep(self.timeToRun)
         else:
-            self.turnServo[self.channel].throttle = -0.2
+            # self.turnServo[self.channel].throttle = -0.2
             print("Turning servo backward...")
             time.sleep(self.timeToRun)
 
-        self.turnServo[self.channel].throttle = 0.0
-        return
+        # self.turnServo[self.channel].throttle = 0.0
+        return True
     
-    def calculateSpiceTime(self, amount: float, size: str, teaspoonsPerSecond: float):
-        if size == "Tablespoons":
-            return (amount * 3) * teaspoonsPerSecond
-        elif size == "Cups":
-            return (amount * 48) * teaspoonsPerSecond
-        else:
-            return 0
-        
     def getRecipes(self):
         self.recipe = {}
         with open('recipes.txt', 'r') as f:
