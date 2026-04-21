80c64d4146f6f809acfa0c4a9a46c82eec520eb6
Jacob Partridge <jppeabody83@gmail.com>
2026-04-20 20:16:10 -0500

    asdf

--- calibrateServo.py
+++ calibrateServo.py
@@ -1,11 +1,11 @@
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 channel = 0
-input = ''
+userInput = ''
 turnServo = kit.continuous_servo[channel]
 
-while input != 'exit':
-    input = input("-1 to 1")
-    turnServo.throttle = float(input)
+while userInput != 'exit':
+    userInput = input("-1 to 1")
+    turnServo.throttle = float(userInput)
 
 turnServo.throttle = 0
