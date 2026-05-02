9e60f56e12c93ad66fcab747b19876cd0d09eaf7
Jacob <jepartridge@shockers.wichita.edu>
2026-05-02 12:41:59 -0500

    asdf

--- backEnd.py
+++ backEnd.py
@@ -122,7 +122,7 @@ class SpiceItUpBackend:
             # Now each thread references its own local channel and timeToRun
             self.turnServo[channel].throttle = .2
             time.sleep(timeToRun)
-            self.turnServo[channel].throttle = 0.5
+            self.turnServo[channel].throttle = 0.6
             return
 
         except Exception as e:
