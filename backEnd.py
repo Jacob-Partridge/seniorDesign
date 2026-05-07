5fd5be145854de0b613a7eeaea24fd4ec4f95b93
Jacob <jepartridge@shockers.wichita.edu>
2026-05-06 20:21:35 -0500

    Salt flow rate = 10 seconds

--- backEnd.py
+++ backEnd.py
@@ -25,11 +25,11 @@ class SpiceItUpBackend:
             # the other spices, We can adjust the flow rates as we test the
             # machine and get more accurate measurements for each spice
 
-            self.spices = {'Salt': {"teaspoons/second": 7.5,
+            self.spices = {'Salt': {"teaspoons/second": 10,
                                     "currentlyHoused": -1,
                                     "conversionConstant": 1},
 
-                           'Black Pepper': {"teaspoons/second": 7.5,
+                           'Black Pepper': {"teaspoons/second": 10,
                                             "currentlyHoused": -1,
                                             "conversionConstant": 1},
 
