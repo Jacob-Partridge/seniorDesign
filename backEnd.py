b63995326ff4ba2b8c15aa9db0775fd70bd8315f
Jacob <jepartridge@shockers.wichita.edu>
2026-05-07 15:37:07 -0500

    salt, pepper, sugar = 8.5

--- backEnd.py
+++ backEnd.py
@@ -25,11 +25,11 @@ class SpiceItUpBackend:
             # the other spices, We can adjust the flow rates as we test the
             # machine and get more accurate measurements for each spice
 
-            self.spices = {'Salt': {"teaspoons/second": 7.5,
+            self.spices = {'Salt': {"teaspoons/second": 8.5,
                                     "currentlyHoused": -1,
                                     "conversionConstant": 1},
 
-                           'Black Pepper': {"teaspoons/second": 7.5,
+                           'Black Pepper': {"teaspoons/second": 8.5,
                                             "currentlyHoused": -1,
                                             "conversionConstant": 1},
 
@@ -61,7 +61,7 @@ class SpiceItUpBackend:
                                              "currentlyHoused": -1,
                                              "conversionConstant":  1},
 
-                           'Sugar': {"teaspoons/second":  7.5,
+                           'Sugar': {"teaspoons/second":  8.5,
                                            "currentlyHoused": -1,
                                            "conversionConstant":  1}
                            }
